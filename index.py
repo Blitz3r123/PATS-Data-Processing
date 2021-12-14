fe4e24dfb9c06c1a740c94eccea2d9d1bae4e7a8
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2021-12-14 19:31:23 +0000

    Initial commit

---
+++ index.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from rich.console import Console
+
+console = Console()
+
+plt.close("all")
+
+def getLatencyCol(dataframe):
+    """
+    Returns the name of the column that has "latency" in it.
+
+    :param dataframe: data where the latency column resides
+    :return: returns column title that contains "latency"
+    """
+    for col in df.columns.values.tolist():
+        if "latency" in col.lower():
+            latency_col_title = col
+
+    if not latency_col_title:
+        console.print("""
+    [yellow]WARNING:[/yellow] Latency not found in any column.
+        """)
+        return ""
+    else:
+        return latency_col_title
+
+raw_df = pd.read_csv('base_case_multicast/run_1/vm1_output/pub_output.csv')
+row_count = len(raw_df)
+
+# Skip first 4 rows
+df = pd.read_csv('base_case_multicast/run_1/vm1_output/pub_output.csv', skiprows=4)
+
+# Choose which rows and columns to include:
+# [row_start:row_end, col_start: col_end]
+df = df.iloc[0:(row_count - 6), 0:6]
+
+latency_col_title = getLatencyCol(df)
+
+latencies = df[latency_col_title]
+
+console.print(latencies.to_string())
+
+# df.plot()
